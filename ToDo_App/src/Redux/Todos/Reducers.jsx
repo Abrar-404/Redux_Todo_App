7a19f5b3d2d00e98c77fdf920f1e7c07e43a745a
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-19 11:47:43 +0600

    added state id

--- ToDo_App/src/Redux/Todos/Reducers.jsx
+++ ToDo_App/src/Redux/Todos/Reducers.jsx
@@ -1,3 +1,24 @@
 import { InitialState } from "./InitialState"
+import {ADDED, ALLCOMPLETED, CLEARCOMPLETED, TOGGLED, DELETED, COLORSELECTED, } from './ActionTypes'
 
-const reducer = (state = InitialState, action) => {};
+
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
+const reducer = (state = InitialState, action) => {
+  switch (action.type) {
+    case ADDED:
+
+      return [
+        ...state,
+        {
+          id: nextTodoId(state)
+        }
+      ]
+
+    default:
+      break;
+  }
+};
