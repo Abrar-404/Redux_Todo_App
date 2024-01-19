8ed943bd58e7b732962fb849ce06444010efe48e
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-19 11:14:30 +0600

    added clear completed

--- ToDo_App/src/Redux/Todos/ActionTypes.jsx
+++ ToDo_App/src/Redux/Todos/ActionTypes.jsx
@@ -3,3 +3,4 @@ export const TOGGLED = 'todos/toggled';
 export const COLORSELECTED = 'todos/colorSelcted';
 export const DELETED = 'todos/deleted';
 export const ALLCOMPLETED = 'todos/allCompleted';
+export const CLEARCOMPLETED = 'todos/clearCompleted';
