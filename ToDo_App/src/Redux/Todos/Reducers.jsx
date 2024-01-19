51cb6b95846c4927a5bd119a9b15a2fa948a9a03
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-19 11:50:59 +0600

    added toggled

--- ToDo_App/src/Redux/Todos/Reducers.jsx
+++ ToDo_App/src/Redux/Todos/Reducers.jsx
@@ -1,22 +1,38 @@
-import { InitialState } from "./InitialState"
-import {ADDED, ALLCOMPLETED, CLEARCOMPLETED, TOGGLED, DELETED, COLORSELECTED, } from './ActionTypes'
+import { InitialState } from './InitialState';
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  TOGGLED,
+  DELETED,
+  COLORSELECTED,
+} from './ActionTypes';
 
-
-const nextTodoId = (todos) => {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
+const nextTodoId = todos => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+  return maxId + 1;
+};
 
 const reducer = (state = InitialState, action) => {
   switch (action.type) {
     case ADDED:
-
       return [
         ...state,
         {
-          id: nextTodoId(state)
+          id: nextTodoId(state),
+        },
+      ];
+
+    case TOGGLED:
+      return state.map(todo => {
+        if (todo.id !== action.payload) {
+          return todo;
         }
-      ]
+        return {
+          ...todo,
+          completed: !completed,
+        };
+      });
 
     default:
       break;
