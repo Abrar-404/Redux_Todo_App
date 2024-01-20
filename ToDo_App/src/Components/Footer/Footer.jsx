e2ff0b713b5108721ca9ea6d0830781f87dd91b1
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-20 17:18:25 +0600

    added footer filters

--- ToDo_App/src/Components/Footer/Footer.jsx
+++ ToDo_App/src/Components/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import { useSelector } from 'react-redux';
 const Footer = () => {
+  const todos = useSelector(state => state.todos);
+  const todosRemaining = todos.filter(todo => !todo.completed).length;
+
   return (
     <div>
       <div className="mt-4 flex justify-between text-xs text-gray-500">
-        <p>2 tasks left</p>
+        <p>{todosRemaining} tasks left</p>
         <ul className="flex space-x-1 items-center text-xs">
           <li className="cursor-pointer font-bold">All</li>
           <li>|</li>
