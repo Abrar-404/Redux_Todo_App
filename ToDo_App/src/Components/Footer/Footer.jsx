8c9b0e93cbfd986708a7f7537aca4dbe80b4d97c
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-20 17:27:03 +0600

    added status change in footer

--- ToDo_App/src/Components/Footer/Footer.jsx
+++ ToDo_App/src/Components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { statusChanged } from '../../Redux/Filters/Actions';
 
 const numberOfTodos = no_of_todos => {
   switch (no_of_todos) {
@@ -15,18 +16,38 @@ const numberOfTodos = no_of_todos => {
 
 const Footer = () => {
   const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
   const todosRemaining = todos.filter(todo => !todo.completed).length;
+
+  const handleStatusChange = status => {
+    dispatch(statusChanged(status));
+  };
 
   return (
     <div>
       <div className="mt-4 flex justify-between text-xs text-gray-500">
         <p>{numberOfTodos(todosRemaining)} left</p>
         <ul className="flex space-x-1 items-center text-xs">
-          <li className="cursor-pointer font-bold">All</li>
+          <li
+            onClick={() => handleStatusChange('All')}
+            className="cursor-pointer font-bold"
+          >
+            All
+          </li>
           <li>|</li>
-          <li className="cursor-pointer">Incomplete</li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleStatusChange('Incomplete')}
+          >
+            Incomplete
+          </li>
           <li>|</li>
-          <li className="cursor-pointer">Complete</li>
+          <li
+            className="cursor-pointer"
+            onClick={() => handleStatusChange('Complete')}
+          >
+            Complete
+          </li>
           <li></li>
           <li></li>
           <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
