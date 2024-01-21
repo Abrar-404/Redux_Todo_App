b75577dba53b4ce8a443d97142f3ac69a41e1360
Sahil Abrar <zeroxkhan2@gmail.com>
2024-01-21 17:37:51 +0600

    filtered by status

--- ToDo_App/src/Pages/TodoList/TodoList.jsx
+++ ToDo_App/src/Pages/TodoList/TodoList.jsx
@@ -3,12 +3,27 @@ import { useSelector } from 'react-redux';
 
 const TodoList = () => {
   const todos = useSelector(state => state.todos);
+  const filters = useSelector(state => state.filters);
   return (
     <div>
       <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-        {todos?.map(todo => (
-          <TodoApp todo={todo} key={todo.id}></TodoApp>
-        ))}
+        {todos
+          ?.filter(todo => {
+            const { status } = filters;
+            switch (status) {
+              case 'Complete':
+                return todo.completed;
+
+              case 'Incomplete':
+                return !todo.completed;
+
+              default:
+                return true;
+            }
+          })
+          .map(todo => (
+            <TodoApp todo={todo} key={todo.id}></TodoApp>
+          ))}
       </div>
     </div>
   );
